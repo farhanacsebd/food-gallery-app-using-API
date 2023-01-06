a42faeb87592a4c57c0c745ec71533e4dd121ba7
farhanacsebd <farhana.cse.bd@gmail.com>
2023-01-07 01:43:25 +0600

    added card

--- app/app.js
+++ app/app.js
@@ -1,3 +1,19 @@
-function searchFood(){
-    console.log('hello');
+const searchFood = () => {
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    console.log(searchText);
+    searchField.value = '';
+
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displaySearchFood(data.meals))
+}
+
+const displaySearchFood = datas =>{
+    // console.log(datas);
+    const foodCard = document.getElementById('foodCard');
+    datas.forEach(data => {
+        console.log(data);
+    });
 }
