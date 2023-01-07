0a9f98b222f1885df9be6b9b42718b0b589f4ae1
farhanacsebd <farhana.cse.bd@gmail.com>
2023-01-07 10:29:13 +0600

    added sarching product

--- app/app.js
+++ app/app.js
@@ -11,9 +11,24 @@ const searchFood = () => {
 }
 
 const displaySearchFood = datas =>{
-    // console.log(datas);
+    
     const foodCard = document.getElementById('foodCard');
     datas.forEach(data => {
         console.log(data);
+        const div = document.createElement('div')
+        div.classList.add('col')
+        div.innerHTML = 
+        `
+        <div class="col shadow">
+              <div class="card h-100">
+                <img  style="height: 250px;width: 300px;" src="${data.strMealThumb}" class="card-img-top p-2" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title text-center">${data.strMeal}</h5>
+                  <p class="card-text">${data.strInstructions.slice(0,150)}</p>
+                </div>
+              </div>
+            </div>
+        `
+        foodCard.appendChild(div)
     });
 }
