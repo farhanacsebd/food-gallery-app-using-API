de7f468b5451fac7bb7eecbe211e8897cc503869
farhanacsebd <farhana.cse.bd@gmail.com>
2023-01-07 14:47:57 +0600

    complete card details

--- app/app.js
+++ app/app.js
@@ -1,3 +1,4 @@
+// searching input
 const searchFood = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
@@ -10,6 +11,8 @@ const searchFood = () => {
         .then(data => displaySearchFood(data.meals))
 }
 
+
+// display the card
 const displaySearchFood = datas =>{
     
     const foodCard = document.getElementById('foodCard');
@@ -20,15 +23,45 @@ const displaySearchFood = datas =>{
         div.innerHTML = 
         `
         <div class="col shadow">
-              <div class="card h-100">
+              <div class="card h-100 text-center">
                 <img  style="height: 250px;width: 300px;" src="${data.strMealThumb}" class="card-img-top p-2" alt="...">
                 <div class="card-body">
-                  <h5 class="card-title text-center">${data.strMeal}</h5>
-                  <p class="card-text">${data.strInstructions.slice(0,150)}</p>
+                  <h5 class="card-title mb-3">${data.strMeal}</h5>
+                  <!-- Button trigger modal -->
+                  <button  onclick="detailsInfo(${data.idMeal})" type="button" class="btn btn-danger rounded-pill text-white fw-bold p-2" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                   Get Recipe
+                  </button>
                 </div>
               </div>
             </div>
         `
         foodCard.appendChild(div)
     });
+}
+
+const detailsInfo = idMeal =>{
+  // console.log(idMeal);
+  const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`
+  fetch(url)
+  .then(res => res.json())
+  .then(data => displayDetailsInfo(data.meals[0]))
+}
+
+const displayDetailsInfo = meals =>{
+      console.log(meals);
+      const ditailInfo = document.getElementById('ditailInfo');
+      const div = document.createElement('div')
+      div.classList.add('modal-body')
+      div.innerHTML =
+      `
+          <img style="height: 150px;" class="rounded-circle mb-3" src="${meals.strMealThumb}" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h4>${meals.strMeal}</h4>
+            <h6 class="bg-white p-2 text-danger fw-bold">Famous in ${meals.strArea}</h6>
+            <h5>Instructions:</h5>
+            <p class="card-text fw-bold">${meals.strInstructions.slice(0,200)}</p>
+            <a href="${meals.strYoutube}" class="btn btn-danger">Watch Video</a>
+          </div>
+      `
+      ditailInfo.appendChild(div)
 }
